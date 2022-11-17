1705324c1ce6aa38bf870bd27a6d946441173e37
Navneetkushwaha <116109592+Navneetkushwaha@users.noreply.github.com>
2022-11-17 20:36:44 +0530

    Create 01-BinarySearch.py

---
+++ 01-BinarySearch.py
@@ -0,0 +1,16 @@
+def Binary_Search(arr,target):
+  low = 0
+  high = len(arr)-1
+  
+  while low<=high:
+    
+    mid = low + (high-low)//2
+    
+    if arr[mid] == target:
+      return mid
+    elif arr[mid] > target:
+      high = mid - 1
+    else:
+      low = mid + 1
+      
+  return -1
